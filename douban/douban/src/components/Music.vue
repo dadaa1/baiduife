3580909e4aeb822f46438e1420676c1faddea2ff
dadaa <1692972759@qq.com>
2017-03-15 12:07:35 +0800

    4444555

---
+++ douban/douban/src/components/Music.vue
@@ -0,0 +1,40 @@
+<template>
+	<audio autoplay :src="dizhi" id='music'
+	loop='true'></audio>
+</template>
+
+<script>
+	export default{
+		name:'music',
+		props: ['dizhi'],
+		data(){
+			return {
+
+			}
+		},
+		methods:{
+			zanting:function(){
+				document.getElementById('music').pause();
+			},
+			xiayishou:function(){
+
+			},
+			shangyishou:function(){
+
+			},
+			xuhuan:function(){
+
+			},
+			timePlay:function(){
+
+			}
+
+		}
+	}
+</script>
+
+<style>
+	audio{
+		display: none;
+	}
+</style>
