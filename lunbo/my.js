c16ef697986b1d302135f40ccab2a963b0e0e250
dadaa <1692972759@qq.com>
2017-03-26 20:14:07 +0800

    1236544

---
+++ lunbo/my.js
@@ -0,0 +1,11 @@
+var angle=90-180/n;
+var huodu=angle*Math.PI;
+var w=400;//图片的宽度
+
+function rotateImg(arr){
+	var x=w/2+w*Math.cos(huodu)/2;
+	var y=0;
+	var z=w*Math.sin(huodu)/2;
+	var rotate=-angle;
+	
+}
